8620d3a169aeb80acc66c0e1b987cafa89cf673d
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-04-10 14:39:53 -0400

    Create Negative.java

---
+++ Negative.java
@@ -0,0 +1,26 @@
+/* Sebastian Richardson
+    4/10/2024
+    Negative.java */
+
+
+
+
+public class Negative {
+    public static void main (String[] args){
+        int num1 = (int)(7*Math.random() -3);
+        int num2 = (int)(7*Math.random() -3);
+        int num3 = (int)(7*Math.random() -3);
+        int num4 = (int)(7*Math.random() -3);
+        int sum = num1 + num2 + num3 + num4;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(sum);
+
+
+    }
+}
+
+
